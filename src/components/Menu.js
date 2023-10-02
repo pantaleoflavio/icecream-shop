9175dd30544094cf95d687b7c942331b945eea85
flavio <flavio.pantaleo@yahoo.com>
2023-10-02 19:38:58 +0200

    refactoring, add new States in Menu component, fetch from a real api

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,27 +6,73 @@ import data from "../fakeData";
 const url = "https://react--course-api.herokuapp.com/api/v1/data/gelateria";
 
 const Menu = () => {
+  // loading state for data fetching
+  const [isLoading, setIsLoading] = useState(true);
+
+  // error handling state
+  const [isError, setIsError] = useState(false);
+
+  // whole products
   const [prodotti, setProdotti] = useState(data);
+
+  // active btn selector
   const [selected, setSelected] = useState(0);
+
+  // products fltered frpm products state
   const [filterProducts, setFilterProducts] = useState(prodotti);
 
-  const categorie = Array.from(new Set(prodotti.map( el => el.categoria)));
-  categorie.unshift("all");
+  // categori products
+  const [categorie, setCategorie] = useState([]);
+
+  // const categorie = Array.from(new Set(prodotti.map( el => el.categoria)));
+  // categorie.unshift("all");
 
   const filterProdotti = (categoria, index) => {
     setSelected(index);
     if (categoria === "all") {
-      setFilterProducts(data);
+      setFilterProducts(prodotti);
     } else {
       setFilterProducts(prodotti.filter(el => el.categoria === categoria ? el : "" ))
     }
-  }
+  };
 
-  return <div className="container">
+  React.useEffect(() => {
+    // IIFE
+    (async()=>{
+      setIsLoading(true);
+      setIsError(false);
+      try {
+        const response = await axios.get(url);
+        setProdotti(response.data.data);
+        setFilterProducts(response.data.data);
+
+        //Categories array
+        const nuoveCategorie = Array.from(
+          new Set(response.data.data.map( el => el.categoria))
+          );
+        nuoveCategorie.unshift("all");
+
+        setCategorie(nuoveCategorie);
+        
+        setIsLoading(false);
+        setIsError(false);
+      } catch (e) {
+        console.log(e);
+        setIsLoading(false);
+        setIsError(true);
+      }
+    })()
+  }, []);
+
+  return ( 
+  <div className="container">
     <h4 style={{textAlign: "center", textTransform: "uppercase"}}>
       le nostre scelte
     </h4>
-    <div className="lista-categorie">
+    {
+      !isLoading && !isError ? (
+      <>
+        <div className="lista-categorie">
       {
         categorie.map((categoria, index) => {
           return <button
@@ -40,11 +86,34 @@ const Menu = () => {
       }
     </div>
     <div className="vetrina">
-      {
-        filterProducts.map(el => <Gelato key={el.id} {...el} />)
-      }
+      {filterProducts.map(el => (
+        <Gelato key={el.id} {...el} />
+        ))}
     </div>
-  </div>;
+      </>
+      ) : !isLoading && isError ? (
+        <h4 style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+        >
+          Error...
+        </h4>
+        ) : (
+          <h4 style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+          >
+            Loading...
+          </h4>
+      )}
+    </div>
+  );
 };
 
 export default Menu;
